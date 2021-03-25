ae5760e402d91a95e57529a9ce3e97aa25fa9631
Anastasiya1982 <52328409+Anastasiya1982@users.noreply.github.com>
2021-03-25 13:39:19 +0300

    Add files via upload

---
+++ src/components/Preloader/preloader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import preloader from './loading.gif';
+
+const Preloader =()=>{
+    return <div>
+        <img src={preloader} alt='preloader'/>
+    </div>
+};
+
+export default Preloader
